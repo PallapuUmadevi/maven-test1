9098a6ff14b9e6e2ac003d14996e26720d878134
PallapuUmadevi <uumadevi736@gmail.com>
2025-08-19 12:23:08 +0530

    UMAD0001 - Wrote code to check if number is odd or even

---
+++ src/main/java/com/example/javamavenjunittesthelloworld/src/main/java/com/example/javamavenjunittesthelloworld/Utility.java
@@ -0,0 +1,11 @@
+package com.example.javamavenjunittesthelloworld;
+
+public class Utility {
+    public static void UmadeviOddEven(int num) {
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
